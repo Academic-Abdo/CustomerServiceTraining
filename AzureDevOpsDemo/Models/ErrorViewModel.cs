e661d572b811867d1840f2d045662fac1f4b54db
Kabdulraoof <abdulraoofalkhawga@gmail.com>
2024-06-11 22:57:08 +0300

    Add projact Files

---
+++ AzureDevOpsDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureDevOpsDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
